d887b88734856b7f3988316cb35cb796b9a5635a
Christopher Brown <io@henrian.com>
2017-02-18 14:50:52 -0600

    Delete twitter.api.search/search which is already implemented at twitter.api.restful/search-tweets

--- test/twitter/test/api/search.clj
+++
@@ -1,7 +0,0 @@
-(ns twitter.test.api.search
-  (:require [clojure.test :refer :all]
-            [twitter.api.search :refer :all]
-            [twitter.test-utils :refer [is-200]]))
-
-(deftest test-search
-  (is-200 search :params {:q "sports"}))
