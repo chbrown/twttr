cdd293d71bd0fc3f713708f133f3986a4a791fd2
Christopher Brown <io@henrian.com>
2017-12-14 16:14:51 -0600

    Move codox plugin & config into :dev profile

--- project.clj
+++ project.clj
@@ -7,10 +7,11 @@
                  [org.clojure/data.json "0.2.6"]
                  [aleph "0.4.4"]
                  [clj-oauth "1.5.5"]]
-  :codox {:exclude-vars nil
-          :source-paths ["src"]
-          :source-uri "https://github.com/chbrown/twttr/blob/{version}/{filepath}#L{line}"}
   :profiles {:uberjar {:aot :all}
              :dev {:dependencies [[org.clojure/tools.namespace "0.3.0-alpha3"]]
+                   :plugins [[lein-codox "0.10.3"]]
+                   :codox {:exclude-vars nil
+                           :source-paths ["src"]
+                           :source-uri "https://github.com/chbrown/twttr/blob/{version}/{filepath}#L{line}"}
                    :source-paths ["dev" "src"]
                    :repl-options {:init-ns user}}})
