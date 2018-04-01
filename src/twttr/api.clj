69dc049cd8208da7b3cefd0e98cdcfce9c0654b1
Christopher Brown <io@henrian.com>
2018-04-01 18:42:26 -0500

    Extract middleware out of 'api' module

--- src/twttr/api.clj
+++ src/twttr/api.clj
@@ -1,10 +1,9 @@
 (ns twttr.api
   (:require [clojure.string :as str]
             [clojure.data.json :as json]
-            [clojure.java.io :as io]
             [aleph.http :as http]
-            [byte-streams :as bs]
             [manifold.deferred :as d]
+            [twttr.middleware :refer [wrap-rest-middleware wrap-stream-middleware]]
             [twttr.endpoints :as endpoints]
             [twttr.auth :refer [auth-header]]))
 
@@ -29,10 +28,6 @@
     (ex-info (->> message-parts (remove nil?) (str/join " ")) response)))
 
 (defn request
-  "the argument to middleware should be a function that takes a handler
-  (which turns a request into a response), and returns a function which takes a request,
-  potentially modifies that request, runs request through the given handler to get a response,
-  and then returns the response, potentially after modifying it."
   [request-method url query-params authorization options]
   (-> options
       (assoc :request-method request-method :url url :query-params query-params)
@@ -42,29 +37,6 @@
                (fn [ex]
                  (throw (ex-twitter (ex-data ex)))))
       (deref)))
-
-(defn- wrap-rest-middleware
-  "REST middleware for parsing response body as single JSON document"
-  [handler]
-  (fn rest-middleware-handler [request]
-    (d/chain (handler request)
-             (fn [response]
-               (with-meta (-> (:body response)
-                              (io/reader)
-                              (json/read :key-fn keyword :eof-error? false)) response)))))
-
-(defn- wrap-stream-middleware
-  "Streaming middleware for parsing response as infinite sequence of JSON documents,
-  separated by newlines"
-  [handler]
-  (fn stream-middleware-handler [request]
-    (d/chain (handler request)
-             (fn [response]
-               (with-meta (->> (:body response)
-                               (bs/to-line-seq)
-                               ; Twitter will send lots of newlines as keep-alive signals if the stream is sparse
-                               (remove empty?)
-                               (map #(json/read-str % :key-fn keyword))) response)))))
 
 (defn request-endpoint
   [endpoint credentials options]
---
+++ src/twttr/middleware.clj
@@ -0,0 +1,34 @@
+(ns twttr.middleware
+  (:require [clojure.data.json :as json]
+            [clojure.java.io :as io]
+
+            [byte-streams :as bs]
+            [manifold.deferred :as d]))
+
+; The argument to middleware should be a function that takes a handler
+; (which turns a request into a response), and returns a function which takes a request,
+; potentially modifies that request, runs request through the given handler to get a response,
+; and then returns the response, potentially after modifying it.
+
+(defn wrap-rest-middleware
+  "REST middleware for parsing response body as single JSON document"
+  [handler]
+  (fn rest-middleware-handler [request]
+    (d/chain (handler request)
+             (fn [response]
+               (with-meta (-> (:body response)
+                              (io/reader)
+                              (json/read :key-fn keyword :eof-error? false)) response)))))
+
+(defn wrap-stream-middleware
+  "Streaming middleware for parsing response as infinite sequence of JSON documents,
+  separated by newlines"
+  [handler]
+  (fn stream-middleware-handler [request]
+    (d/chain (handler request)
+             (fn [response]
+               (with-meta (->> (:body response)
+                               (bs/to-line-seq)
+                               ; Twitter will send lots of newlines as keep-alive signals if the stream is sparse
+                               (remove empty?)
+                               (map #(json/read-str % :key-fn keyword))) response)))))
