fc326e4d120a678d7c1f8be42ce383339935895d
Christopher Brown <io@henrian.com>
2018-04-08 17:00:02 -0500

    Increment pre-release version to beta4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject twttr "3.0.0-beta3"
+(defproject twttr "3.0.0-beta4"
   :description "Twitter API client supporting REST, streaming, and OAuth"
   :url "https://github.com/chbrown/twttr"
   :license {:name "Eclipse Public License"
