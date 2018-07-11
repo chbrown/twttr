e851b8ec6d3124a118f622b235c3a4a5492b69ed
Christopher Brown <io@henrian.com>
2018-07-11 13:14:07 -0500

    Version 3.2.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject twttr "3.2.0"
+(defproject twttr "3.2.1"
   :description "Twitter API client supporting REST, streaming, and OAuth"
   :url "https://github.com/chbrown/twttr"
   :license {:name "Eclipse Public License"
